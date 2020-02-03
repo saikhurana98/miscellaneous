b8df53a2cb4a4259282702490e74764f43065cf4
Sai Khurana <sai.khurana_ug21@ashoka.edu.in>
2020-02-03 19:28:58 +0530

    updated script for sinric pro

---
+++ app.py
@@ -0,0 +1,133 @@
+from sinric import SinricPro
+from sinric import SinricProUdp
+from credentials import appKey, secretKey, deviceIdArr, blynkIdArr, pwrPinArr, rgbPinArr, slidePinArr
+from time import sleep
+import requests
+
+def onPowerState(did, state):
+    # Alexa, turn ON/OFF Device
+    index = deviceIdArr.index(did)
+    changePwrState(blynkIdArr[index], pwrPinArr[index], state)
+    return True, state
+
+def onSetBrightness(did, state):
+    # Alexa set device brightness to 40%
+    print(did, 'BrightnessLevel : ', state)
+    return True, state
+
+
+def onAdjustBrightness(did, state):
+    # Alexa increase/decrease device brightness by 44
+    index = deviceIdArr.index(did)
+    changeBrighness(blynkIdArr[index], slidePinArr[index], state)
+    return True, state
+
+
+def onSetColor(did, r, g, b):
+    # Alexa set device color to Red/Green
+    index = deviceIdArr.index(did)
+    writeColor(blynkIdArr[index], rgbPinArr[index], [r, g, b])
+    return True
+
+
+def onSetColorTemperature(did, value):
+    index = deviceIdArr.index(did)
+    writeColor(blynkIdArr[index], rgbPinArr[index], [255, 255, 255])
+    return True
+
+
+def onIncreaseColorTemperature(deviceId, value):
+    return True, value
+
+
+def onDecreaseColorTemperature(deviceId, value):
+    return True, value
+
+
+def Events():
+    # client.event_handler.raiseEvent(switchId, 'setPowerState',data={'state': 'On'})
+    pass
+
+
+event_callback = {
+    'Events': Events
+}
+
+callbacks = {
+    'powerState': onPowerState,
+    'setBrightness': onSetBrightness,
+    'adjustBrightness': onAdjustBrightness,
+    'setColor': onSetColor,
+    'setColorTemperature': onSetColorTemperature,
+    'increaseColorTemperature': onIncreaseColorTemperature,
+    'decreaseColorTemperature': onDecreaseColorTemperature
+}
+
+
+def changePwrState(blynk_auth, pin, state):
+    url = f'http://188.166.206.43/{blynk_auth}/update/{pin}'
+
+    if state == 'On':
+        state = 1
+    else:
+        state = 0
+    print(state)
+    querystring = {"value":state}
+
+    headers = {
+        'User-Agent': "PostmanRuntime/7.18.0",
+        'Accept': "*/*",
+        'Host': "blynk-cloud.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+def changeBrighness(blynk_auth, pin, state):
+    url = f'http://188.166.206.43/{blynkIdArr}/update/{pin}'
+
+    querystring = {"value":state}
+
+    headers = {
+        'User-Agent': "PostmanRuntime/7.18.0",
+        'Accept': "*/*",
+        'Host': "blynk-cloud.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+def writeColor(blynk_auth, pin,rgb):
+    url = f'http://188.166.206.43/{blynk_auth}/update/{pin}'
+    print(url)
+    querystring = {"value":rgb}
+
+    headers = {
+        'User-Agent': "PostmanRuntime/7.18.0",
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "d001f97e-0181-4e40-8518-56d8890142ec,256ed8fa-93bb-4a16-ad34-96929283b2b0",
+        'Host': "blynk-cloud.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+
+if __name__ == '__main__':
+    client = SinricPro(appKey, deviceIdArr, callbacks,
+        event_callbacks=event_callback, enable_log=False,restore_states=True,secretKey=secretKey)
+    udp_client = SinricProUdp(callbacks,deviceIdArr,enable_trace=False)  # Set it to True to start logging request Offline Request/Response
+    client.handle_all(udp_client)
---
+++ credentials.py
@@ -0,0 +1,17 @@
+# Sinric Credentials
+appKey = '61de13fe-c2b7-48ef-a90a-9adb9d3a5305'
+secretKey='b4286c7f-4c70-4150-a115-3d814afd65ce-d43daabd-5be1-4675-b7c7-068eb5004d3a'
+switchId='5e3261fcf9370215857f9833'
+light1Id='5e3261a0fe4eb5157d6949eb'
+light2Id='5e386b13e2e1685360b4bf5b'
+
+deviceIdArr = [switchId, light1Id, light2Id]
+
+# Blynk Credentials
+switchBlynkId = '9cfd20fc87ee48aca8dd6c771791f43b'
+light1BlynkId = 'ZPbgCab2ZVSZi-M54vroCC-d8mjf0Jf2'
+light2BlynkId = '53f4b84a31fb44a19941497db01ae5af'
+blynkIdArr = [switchBlynkId, light1BlynkId, light2BlynkId]
+pwrPinArr = ['v2', 'v0', 'v0']
+slidePinArr = [None, 'v5', 'v5']
+rgbPinArr = [None,'v1', 'v1']
